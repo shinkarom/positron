c60d528c017668447f6fb7adc8ede9504e8ad90d
shinkarom <shinkarom@gmail.com>
2025-05-07 11:06:24 +0300

    Add posiSDLTick function

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ uint64_t targetTime;
 constexpr SDL_AudioSpec spec = {.format = SDL_AUDIO_S16LE, .channels=2, .freq=audioSampleRate};
 SDL_AudioDeviceID audioID;
 SDL_AudioStream* stream;
+int16_t* audioBuffer;
 
 bool hasFullscreen;
 bool isPaused;
@@ -55,6 +56,82 @@ constexpr SDL_Scancode inputScancodes[numInputButtons] = {
 	SDL_SCANCODE_D,
 };
 
+bool posiSDLTick() {
+	SDL_FRect texRect, winRect;
+	
+	bool done = false;
+	while(SDL_PollEvent(&event)) {
+		if (event.type == SDL_EVENT_QUIT) {
+			done = true;
+		} else if (event.type == SDL_EVENT_KEY_DOWN) {
+			
+		} else if (event.type == SDL_EVENT_KEY_UP) {
+			if(event.key.scancode == SDL_SCANCODE_F9) {
+				posiAPISlotDeleteAll();
+			}
+			if(event.key.scancode == SDL_SCANCODE_F10) {
+				if(!posiReset()){
+					done = true;
+				}
+			}
+			if(event.key.scancode == SDL_SCANCODE_F11){
+				isPaused = !isPaused;
+				if(isPaused) apuClearBuffer();
+			}
+			if((event.key.scancode == SDL_SCANCODE_F12)||(hasFullscreen&&event.key.scancode == SDL_SCANCODE_ESCAPE)) {
+				hasFullscreen = !hasFullscreen;
+				SDL_SetWindowFullscreen(window,hasFullscreen);
+			}
+		}
+	}
+	if(!isPaused) {
+		auto kbState = SDL_GetKeyboardState(nullptr);
+		for (int i = 0; i < numInputButtons; i++) {
+			posiUpdateButton(i, kbState[inputScancodes[i]]);
+		 }
+	}
+	 
+	texRect.x = 0;
+	texRect.y = 0;
+	texRect.w = screenWidth;
+	texRect.h = screenHeight;
+	
+	int windowWidth, windowHeight;
+	SDL_GetCurrentRenderOutputSize(renderer, &windowWidth, &windowHeight);
+	float scaleX = (float)windowWidth / texRect.w;
+	float scaleY = (float)windowHeight / texRect.h;
+	float scale = scaleX < scaleY ? scaleX : scaleY;
+	float destWidth = texRect.w * scale;
+	float destHeight = texRect.h * scale;
+	float destX = (windowWidth - destWidth) / 2.0; // Center horizontally
+	float destY = (windowHeight - destHeight) / 2.0; // Center vertically
+	winRect.x = destX;
+	winRect.y = destY;
+	winRect.w = destWidth;
+	winRect.h = destHeight;
+	
+	
+	 
+	 if(isPaused) {
+		 
+	 } else
+	 if(!posiRun()) {
+		 done = true;
+	 }
+
+	SDL_PutAudioStreamData(stream, audioBuffer, audioFramesPerTick *2 * 2);
+	
+	void* texturePixels;
+	int pitch;
+	SDL_LockTexture(texture, nullptr, &texturePixels, &pitch);
+	posiRedraw((uint32_t*)texturePixels);
+	SDL_UnlockTexture(texture);
+	SDL_RenderClear(renderer);
+	SDL_RenderTexture(renderer, texture, &texRect, &winRect);
+	SDL_RenderPresent(renderer);
+	return done;
+}
+
 int main(int argc, char *argv[])
 {
 	argparse::ArgumentParser program("Positron");
@@ -75,7 +152,7 @@ int main(int argc, char *argv[])
 	hasFullscreen = false;
 	isPaused = false;
 	
-	int16_t* audioBuffer;
+	
 	audioBuffer = posiAudiofeed();
 	auto fileName = program.get<std::string>("file");
 	
@@ -94,7 +171,7 @@ int main(int argc, char *argv[])
 	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, screenWidth, screenHeight);
 	SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
 	
-	SDL_FRect texRect, winRect;
+	
 	
 	audioID = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr);
 	stream = SDL_CreateAudioStream(&spec, nullptr);
@@ -102,75 +179,8 @@ int main(int argc, char *argv[])
 	
 	targetTime = SDL_GetTicks() + msPerTick;
 	while(!done) {	
-		while(SDL_PollEvent(&event)) {
-			if (event.type == SDL_EVENT_QUIT) {
-				done = true;
-			} else if (event.type == SDL_EVENT_KEY_DOWN) {
-				
-			} else if (event.type == SDL_EVENT_KEY_UP) {
-				if(event.key.scancode == SDL_SCANCODE_F9) {
-					posiAPISlotDeleteAll();
-				}
-				if(event.key.scancode == SDL_SCANCODE_F10) {
-					if(!posiReset()){
-						done = true;
-					}
-				}
-				if(event.key.scancode == SDL_SCANCODE_F11){
-					isPaused = !isPaused;
-					if(isPaused) apuClearBuffer();
-				}
-				if((event.key.scancode == SDL_SCANCODE_F12)||(hasFullscreen&&event.key.scancode == SDL_SCANCODE_ESCAPE)) {
-					hasFullscreen = !hasFullscreen;
-					SDL_SetWindowFullscreen(window,hasFullscreen);
-				}
-			}
-		}
-		if(!isPaused) {
-			auto kbState = SDL_GetKeyboardState(nullptr);
-			for (int i = 0; i < numInputButtons; i++) {
-				posiUpdateButton(i, kbState[inputScancodes[i]]);
-			 }
-		}
-		 
-		texRect.x = 0;
-		texRect.y = 0;
-		texRect.w = screenWidth;
-		texRect.h = screenHeight;
+		done = posiSDLTick();
 		
-		int windowWidth, windowHeight;
-		SDL_GetCurrentRenderOutputSize(renderer, &windowWidth, &windowHeight);
-		float scaleX = (float)windowWidth / texRect.w;
-		float scaleY = (float)windowHeight / texRect.h;
-		float scale = scaleX < scaleY ? scaleX : scaleY;
-		float destWidth = texRect.w * scale;
-		float destHeight = texRect.h * scale;
-		float destX = (windowWidth - destWidth) / 2.0; // Center horizontally
-		float destY = (windowHeight - destHeight) / 2.0; // Center vertically
-		winRect.x = destX;
-		winRect.y = destY;
-		winRect.w = destWidth;
-		winRect.h = destHeight;
-		
-		
-		 
-		 if(isPaused) {
-			 
-		 } else
-		 if(!posiRun()) {
-			 done = true;
-		 }
-
-		SDL_PutAudioStreamData(stream, audioBuffer, audioFramesPerTick *2 * 2);
-		
-		void* texturePixels;
-		int pitch;
-		SDL_LockTexture(texture, nullptr, &texturePixels, &pitch);
-		posiRedraw((uint32_t*)texturePixels);
-		SDL_UnlockTexture(texture);
-		SDL_RenderClear(renderer);
-		SDL_RenderTexture(renderer, texture, &texRect, &winRect);
-		SDL_RenderPresent(renderer);
 		auto currentTime = SDL_GetTicks();
 		if(currentTime < targetTime) {
 			SDL_Delay(targetTime - currentTime);
